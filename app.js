260af889d6883aa30ad6c84573c0c338736a093e
vincentp73000 <vincentp73000@gmail.com>
2025-01-21 15:50:12 +0100

    Intégration fichier js méthode fetch

--- app.js
+++ app.js
@@ -1,1 +1,36 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
+const results = document.querySelector(".results");
+
+const research = document.querySelector(".research");
+
+let searchInput = "";
+
+research.addEventListener("input", (e) => {
+  //console.log(e.target.value);
+  searchInput = e.target.value;
+  //console.log(searchInput);
+  retrieveSearch();
+});
+
+function displaySearch(data) {
+    // Itérer un objet en récupérant la clé et la valeur :
+    Object.entries(data.query.search).forEach(([index, value]) =>{
+        console.log(`Clé : ${index}, Valeur : ${value}`);  
+    });
+}
+
+async function retrieveSearch() {
+  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`;
+  const requete = await fetch(url, {
+    //1er url et 2eme : objet
+    method: "GET",
+  });
+  if (!requete.ok) {
+    // On vérifie si erreur
+    alert("Erreur");
+  } else {
+    let donnees = await requete.json(); // recupere les donnees en objet.
+    console.log(donnees);
+    displaySearch(donnees);
+  }
+}
