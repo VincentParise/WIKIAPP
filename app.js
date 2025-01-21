e8aafe39246dfc00cf33c2dbbcf4de997655ec72
vincentp73000 <vincentp73000@gmail.com>
2025-01-21 17:34:45 +0100

    Gestion des erreurs sur le js

--- app.js
+++ app.js
@@ -1,39 +1,65 @@
 // API ENDPOINT : `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`
 
-const results = document.querySelector(".results");
+const form = document.querySelector("form");
+const input = document.querySelector("input");
+const errorMsg = document.querySelector(".error-msg");
+const results = document.querySelector(".results-display");
+const loader = document.querySelector(".loader");
 
-const research = document.querySelector(".research");
-
-let searchInput = "";
-
-research.addEventListener("input", (e) => {
-  //console.log(e.target.value);
-  searchInput = e.target.value;
-  //console.log(searchInput);
-  retrieveSearch();
+// On écoute sur le formulaire
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  if (input.value === "") {
+    errorMsg.textContent = "Wooppss, erreur de saisie";
+    return;
+  } else {
+    errorMsg.textContent = "";
+    loader.style.display = "flex";
+    results.textContent = "";
+    displaySearch(input.value);
+  }
 });
 
-function displaySearch(data) {
-    results.textContent='';
-    // Itérer un objet en récupérant la clé et la valeur :
-    Object.entries(data.query.search).forEach(([index, value]) =>{
-        results.innerHTML += `<p>${value.title}</p></br>`
-        //console.log(`Clé : ${index}, Valeur : ${value.title}`);  
+// On utilise la méthode fetch pour récupérer les données de l'API Wilkipedia
+async function displaySearch(searchInput) {
+  try {
+    // console.log(searchInput);
+    const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`;
+    const requete = await fetch(url, {
+      method: "GET",
     });
+    if (!requete.ok) {
+      // On vérifie si erreur
+      throw new Error(`${requete.status}`);
+    } else {
+      const donnees = await requete.json(); // recupere les donnees en objet.
+      console.log(donnees.query.search);
+      createCard(donnees.query.search);
+    }
+  } catch (error) {
+    errorMsg.textContent = `${error}`;
+    loader.style.display = "none";
+  }
 }
 
-async function retrieveSearch() {
-  const url = `https://en.wikipedia.org/w/api.php?action=query&list=search&format=json&origin=*&srlimit=20&srsearch=${searchInput}`;
-  const requete = await fetch(url, {
-    //1er url et 2eme : objet
-    method: "GET",
+// fonction création dans le DOM
+function createCard(data) {
+    if (!data.length) {
+        errorMsg.textContent="Wops, aucun résultat";
+        loader.style.display="none";
+        return;
+    }
+  data.forEach((el) => {
+    const url = `https://en.wikipedia.org/?curid=${el.pageid}`;
+    const card = document.createElement("div");
+    card.className = "result-item";
+    card.innerHTML = `
+        <h3 class="result-title"><a href="${url}" target="_blank">${el.title}</a></h3>
+        <p><a class="result-link" href="${url}" target="_blank">${url}</a></p>
+        <p class="result-snippet">${el.snippet}</p>
+        `;
+    results.appendChild(card);
   });
-  if (!requete.ok) {
-    // On vérifie si erreur
-    alert("Erreur");
-  } else {
-    let donnees = await requete.json(); // recupere les donnees en objet.
-    console.log(donnees);
-    displaySearch(donnees);
-  }
+  loader.style.display="none";
+  
 }
